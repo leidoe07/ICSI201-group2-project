0295e123703f80ee9dbf5d9ef757b5762dc01f7b
Leilani Dore <leilanidore07@gmail.com>
2026-05-06 12:13:33 -0400

    Merge branch 'main' of https://github.com/leidoe07/ICSI201-group2-project.git into main

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -5,31 +5,61 @@ public class Sudoku {
 	public static void main(String[] args) {
 int[][] grid = new int[9][9];
 Scanner scanner=new Scanner(System.in);
+grid[0][0] = 5; 
+grid[0][1] = 3; 
+grid[0][4] = 7;
+grid[1][0] = 6; 
+grid[1][3] = 1; 
+grid[1][4] = 9; 
+grid[1][5] = 5;
+grid[2][1] = 9; 
+grid[2][2] = 8; 
+grid[2][7] = 6;
+
+while (true) {
+    printGrid(grid);
+    
+    if (isFull(grid)) {
+        System.out.println("YOU WIN!");
+        break;
+    }
+
 		System.out.print("what cell would you like input"
-				+ "\n row :");
+				+ "\n row 0 to 8:");
 		int row =scanner.nextInt();
-		System.out.print("col :");
+		System.out.print("col  0 to 8:");
 		int col=scanner.nextInt();
 		
 		System.out.print("input a value");
 		 int value= scanner.nextInt();
-	 grid[row][col]=value;
-		
-		
-		grid[0][0] = 5;
-		grid[0][1] = 3;
-		grid[1][0] = 6;
-		
-		if (solve(grid)) {
-		    System.out.println("Solved:");
-		} else {
-		    System.out.println("No solution");
-		}
-		printGrid(grid);
+		 if (grid[row][col] != 0) {
+			    System.out.println("Cell already has a value!");
+			} else if (value < 1 || value > 9) {
+			    System.out.println("Please enter a number between 1 and 9.");
+			} else if (isValid(grid, row, col, value)) {
+			    grid[row][col] = value;
+			} else {
+			    System.out.println("\"Invalid move!\"");
+			}
+		 
 
+		 printGrid(grid);
+}
+	
+//		if (solve(grid)) {
+//		    System.out.println("Solved:");
+//		} else {
+//		    System.out.println("No solution");
+//		}
+//		printGrid(grid);
+
+
+	
+		
+		
 	}
-
-	public static void printGrid(int[][] grid) {
+		
+		public static void printGrid(int[][] grid) {
 	    for (int row = 0; row < 9; row++) {
 	        // Every 3 rows, print a horizontal line
 	        if (row % 3 == 0 && row != 0) {
@@ -45,8 +75,9 @@ Scanner scanner=new Scanner(System.in);
 	            // Print the cell value with padding
 	            System.out.print(" " + grid[row][col] + " ");
 	        }
-	        System.out.println();
+	        System.out.println(" ");
 	    }
+	    System.out.println("\n ");
 	}
 	
 		static boolean isValid(int[][] grid, int row, int col, int num) // check if num can be placed at grid[row][col]
@@ -64,7 +95,12 @@ Scanner scanner=new Scanner(System.in);
 		        return true;  // all good moving on
 		    }
 
-
+		public static boolean isFull(int[][] grid) {
+	        for (int r = 0; r < 9; r++)
+	            for (int c = 0; c < 9; c++)
+	                if (grid[r][c] == 0) return false;
+	        return true;
+	    }
 
 		static boolean solve(int[][] grid) { // backtracking solver
 		    for (int row = 0; row < 9; row++) {
